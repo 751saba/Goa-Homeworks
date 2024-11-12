0c1a8130f5545fe2cf63f1fe0a18cad035520da9
751saba <sbolkvadze751@gmail.com>
2024-11-12 16:51:37 +0400

    first commit

---
+++ day_10/classwork/hw.py
@@ -0,0 +1,23 @@
+num1 = float(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = float(input("შეიყვანეთ მეორე რიცხვი: "))
+num3 = float(input("შეიყვანეთ მესამე რიცხვი: "))
+
+print("პირველი რიცხვი:", num1)
+print("მეორე რიცხვი:", num2)
+print("მესამე რიცხვი:", num3)
+
+
+
+
+
+
+
+
+weight = 55.0  # წონა (კგ)
+height = 1.68  # სიმაღლე (მ)
+
+
+bmi = weight / (height ** 2)
+
+
+print( bmi)
---
+++ day_10/homework/hw.py
@@ -0,0 +1,85 @@
+int 
+
+
+5
+-12
+0
+1000
+-50
+7
+199
+-1
+987654321
+25
+
+
+str 
+
+
+"hello"
+"Python"
+"2009"
+"sabba2009"
+"Hello, World!"
+"Saba"
+"hello, how are you?"
+"2024"
+"True"
+"very good!"
+
+
+
+
+3.14
+-2.5
+0.43
+100.45
+-0.99
+7.0
+5.678
+1.12
+123.456789
+-5.5
+
+
+
+a = 10
+b = 5
+c = 15
+d = 10
+e = 20
+f = 7
+g = 25
+h = 3
+i = 8
+j = 10
+
+
+print(a > b)      # True, 10 > 5
+print(c > a)      # True, 15 > 10
+print(d > e)      # False, 10 is not greater than 20
+print(f > h)      # True, 7 > 3
+print(i > j)      # False, 8 is not greater than 10
+print(a > g)      # False, 10 is not greater than 25
+print(e > a)      # True, 20 > 10
+print(b > f)      # False, 5 is not greater than 7
+print(i > f)      # True, 8 > 7
+print(c > g)      # False, 15 is not greater than 25
+
+
+
+
+
+15 = int(input("15:"))
+
+age_in_10_years = 15 + 10
+
+# Printing the result
+print("In 10 years, you will be {age_in_10_years} 15 years old.")
+
+
+
+15 + 20 + 40 = 75
+
+
+75 /3= 25
